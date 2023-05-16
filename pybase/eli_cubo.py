429cc898d2c31fc6862315112940ecdb1d45b637
mazent <massimo.zennaro@texa.com>
2023-05-16 14:29:06 +0200

    rivisto

--- pybase/eli_cubo.py
+++ pybase/eli_cubo.py
@@ -1,6 +1,48 @@
 from pathlib import Path
 
+
 # elimina la roba del cubo dai file c e h
+
+def pin(nomef):
+    # quante macro?
+    macro = []
+    with open(nomef, 'rt') as ing:
+        while True:
+            riga = ing.readline()
+
+            if len(riga) == 0:
+                break
+
+            riga = riga.strip()
+
+            if '#define' in riga:
+                elem = riga.split(' ')
+                if len(elem) == 3:
+                    macro.append([elem[1], elem[2]])
+                continue
+
+    # trovo le due dimensoni massime
+    dim1 = 0
+    for elem in macro:
+        dim = len(elem[0])
+        if dim1 < dim:
+            dim1 = dim
+
+    # salvo
+    with open(nomef + '.pin', 'wt') as usc:
+        for elem in macro:
+            usc.write("#define ")
+            usc.write(elem[0])
+            d1 = len(elem[0])
+            diff = 1 + dim1 - d1
+            s = ' ' * diff
+            usc.write(s)
+            if '_PIN_' in elem[1]:
+                usc.write('( (uint32_t) {} )'.format(elem[1]))
+            else:
+                usc.write(elem[1])
+            usc.write("\n")
+
 
 if __name__ == "__main__":
     p = Path('.')
@@ -25,3 +67,6 @@ if __name__ == "__main__":
         if tolte:
             with open(f, 'wt') as usc:
                 usc.writelines(righe)
+        nomef = str(f)
+        if 'main.h' in nomef:
+            pin(nomef)
