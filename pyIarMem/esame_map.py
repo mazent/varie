c03328f3799b2e547a14d7f695ff77e19bd3b4fc
mazent <massimo.zennaro@texa.com>
2023-09-13 16:16:33 +0200

    stampa ordinata

--- pyIarMem/esame_map.py
+++ pyIarMem/esame_map.py
@@ -1,72 +1,51 @@
 import re
 
-BTSTACK = [
-    'ad_parser.o',
-    'btstack_crypto.o',
-    'btstack_linked_list.o',
-    'btstack_memory.o',
-    'btstack_memory_pool.o',
-    'btstack_run_loop.o',
-    'btstack_util.o',
-    'hci.o',
-    'hci_cmd.o',
-    'hci_dump.o',
-    'hci_transport_h4.o',
-    'l2cap.o',
-    'l2cap_signaling.o',
-    'rfcomm.o',
-    'sdp_server.o',
-    'sdp_util.o',
-    'sdp_client.o',
-    'sdp_client_rfcomm.o',
-    'hfp_ag.o',
-    'hfp.o',
-    'hfp_gsm_model.o',
-    'att_db.o',
-    'att_dispatch.o',
-    'att_server.o',
-    'gatt_client.o',
-    'sm.o',
-    'patchram_BCM20710.o'
+LWIP = [
+    "err.o",
+    "autoip.o",
+    "dhcp.o",
+    "etharp.o",
+    "icmp.o",
+    "ip4.o",
+    "ip4_addr.o",
+    "ip4_frag.o",
+    "def.o",
+    "inet_chksum.o",
+    "init.o",
+    "ip.o",
+    "mem.o",
+    "memp.o",
+    "netif.o",
+    "pbuf.o",
+    "stats.o",
+    "tcp.o",
+    "tcp_in.o",
+    "tcp_out.o",
+    "timeouts.o",
+    "udp.o",
+    "ethernet.o",
 ]
 
-BTPORT = [
-    'bt.port.o',
-    'btport_dump_impl.o',
-    'btport_tlv.o',
-    'btp_hci.o',
-    'btstack_chipset_bcm.o',
-    'central.o',
-    'client.o',
-    'control.o',
-    'dut.o',
-    'hfp_gateway.o',
-    'inquiry.o',
-    'le.adv.bcm.o',
-    'le.adv.btstack.o',
-    'le_link_key_db.o',
-    'ntf_ind.o',
-    'perip.o',
-    'run.loop.o',
-    'spp.o',
-    'spp_conn.o',
-    'spp_link_key_db.o',
-    'varie.o',
-    'cirbu.o',
-    'drv.o',
-    'heap.o',
-    'mail.o',
-    'secure.o',
+PORT = [
+    "drv.o",
+    "lan87xx.o",
+    "lwiport.o",
+    "net_if.o",
+    "net_op.o",
+    "net_op_tcp.o",
+    "net_op_udp.o",
+    "tcp_eco.o",
+    "udp_eco.o",
 ]
 
-SEZ_ROM = ['.rodata', '.text']
-SEZ_RAM = ['.data', '.bss']
+SEZ_ROM = [".rodata", ".text"]
+SEZ_RAM = [".data", ".bss", ".ethernet", ".no_cache", ".dtcm"]
 
 
 def dividi(cosa: str):
     a = cosa.strip()
-    b = re.sub(' +', ' ', a)
-    c = b.split(' ')
+    b = re.sub(" +", " ", a)
+    c = b.split(" ")
     if len(c) >= 6:
         return c
     return None
@@ -86,17 +65,17 @@ def trova(sezioni: list, _riga: str, trovati: list, desiderati: list):
 def placement_summary(nomef: str, lista: list):
     _rom = []
     _ram = []
-    with open(nomef, 'rt') as mappa:
+    with open(nomef, "rt") as mappa:
         while True:
             riga = mappa.readline()
-            if 'PLACEMENT SUMMARY' in riga:
+            if "PLACEMENT SUMMARY" in riga:
                 break
 
         while True:
             riga = mappa.readline()
             if len(riga) == 0:
                 break
-            if 'Unused ranges:' in riga:
+            if "Unused ranges:" in riga:
                 break
             if len(riga) < 50:
                 continue
@@ -115,15 +94,23 @@ def somma(_rxm: list):
     return tot
 
 
-if __name__ == '__main__':
-    NOMEF = 'tracke.map'
+def stampa(_rxm: list):
+    x = sorted(_rxm, key=lambda elem: int(elem[-3], 16))
+    for elem in x:
+        print(elem)
 
-    rom, ram = placement_summary(NOMEF, BTSTACK)
+
+if __name__ == "__main__":
+    NOMEF = "SC756_lwip.map"
+
+    rom, ram = placement_summary(NOMEF, LWIP)
     brom = somma(rom)
     bram = somma(ram)
-    print('BTSTACK occupa: rom {}, ram {}'.format(brom, bram))
+    print("LWIP occupa: rom {}, ram {}".format(brom, bram))
 
-    rom, ram = placement_summary(NOMEF, BTPORT)
+    rom, ram = placement_summary(NOMEF, PORT)
+    stampa(rom)
+    stampa(ram)
     brom = somma(rom)
     bram = somma(ram)
-    print('BTPORT occupa: rom {}, ram {}'.format(brom, bram))
+    print("PORT occupa: rom {}, ram {}".format(brom, bram))
