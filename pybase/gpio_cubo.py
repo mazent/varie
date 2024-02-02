f7a59132de2e6ff9408c26dc6077ce0b15678f83
mazent <massimo.zennaro@texa.com>
2024-02-02 12:51:09 +0100

    anche pin adc

--- pybase/gpio_cubo.py
+++ pybase/gpio_cubo.py
@@ -15,36 +15,45 @@ if __name__ == "__main__":
                 break
 
             if "GPIO_Label=" in riga:
-                pinp = riga.find('.')
+                pinp = riga.find(".")
                 pin = riga[:pinp]
-                nomep = riga.find('=')
-                nome = riga[nomep + 1:]
+                nomep = riga.find("=")
+                nome = riga[nomep + 1 :]
                 nome = nome.strip()
                 ris[pin] = [nome]
                 continue
 
             if ".Signal=GPIO_Output" in riga:
-                pinp = riga.find('.')
+                pinp = riga.find(".")
                 pin = riga[:pinp]
                 dati = ris[pin]
-                dati.append('usc')
+                dati.append("usc")
                 ris[pin] = dati
                 continue
 
             if ".Signal=GPIO_Input" in riga:
-                pinp = riga.find('.')
+                pinp = riga.find(".")
                 pin = riga[:pinp]
                 dati = ris[pin]
-                dati.append('ing')
+                dati.append("ing")
                 ris[pin] = dati
                 continue
 
             if ".GPIO_ModeDefaultEXTI=" in riga or ".Signal=GPXTI" in riga:
-                pinp = riga.find('.')
+                pinp = riga.find(".")
                 pin = riga[:pinp]
                 dati = ris[pin]
                 if len(dati) == 1:
-                    dati.append('irq')
+                    dati.append("irq")
+                    ris[pin] = dati
+                continue
+
+            if ".Signal=ADC" in riga:
+                pinp = riga.find(".")
+                pin = riga[:pinp]
+                dati = ris[pin]
+                if len(dati) == 1:
+                    dati.append("adc")
                     ris[pin] = dati
                 continue
 
@@ -53,17 +62,20 @@ if __name__ == "__main__":
     ing = {}
     usc = {}
     irq = {}
+    adc = {}
     for pin in ris:
         dati = ris[pin]
         if len(dati) == 1:
             continue
-        if dati[1] == 'ing':
+        if dati[1] == "ing":
             ing[pin] = dati[0]
-        if dati[1] == 'usc':
+        if dati[1] == "usc":
             usc[pin] = dati[0]
-        if dati[1] == 'irq':
+        if dati[1] == "irq":
             num = int(pin[2:])
             irq[num] = [pin, dati[0]]
+        if dati[1] == "adc":
+            adc[pin] = dati[0]
 
     with open("gpio.txt", "wt") as gpio:
         if len(usc) == 0:
@@ -86,3 +98,10 @@ if __name__ == "__main__":
             gpio.write("Interruzioni\n")
             for key, value in sorted(irq.items()):
                 gpio.write("    {} {}\n".format(value[0], value[1]))
+
+        if len(adc) == 0:
+            gpio.write("Nessuna adc\n")
+        else:
+            gpio.write("Analogici\n")
+            for pin in adc:
+                gpio.write("    {} {}\n".format(pin, adc[pin]))
